0e7774830b867ff7d7c851b79eb6212ff8313d35
Shreyas <sonureigns007@gmail.com>
2023-09-30 23:01:25 +0530

    consuming search params

--- express/app.js
+++ express/app.js
@@ -11,11 +11,13 @@ const tours = JSON.parse(
 app.get('/', (req, res) => {
   res.send('Welcome to my backend Api store !!!');
 });
+//get request
 app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', data: tours, length: tours.length });
 });
+//post request
 app.post('/api/v1/tours', (req, res) => {
   console.log(req.body);
   const newId = tours.length;
@@ -28,9 +30,24 @@ app.post('/api/v1/tours', (req, res) => {
       console.log(err);
     }
   );
-  res.status(200).send('Data received Successfully');
+  res.status(201).send('Data received Successfully');
 });
-
+//params
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = Number.parseInt(req.params.id);
+  const toursForDuration = tours.filter((ex) => ex.duration === id);
+  if (toursForDuration.length>0) {
+    console.log(toursForDuration);
+    res.status(200).json({
+      status: 'success',
+      data: toursForDuration,
+      length: toursForDuration.length,
+    });
+  } else {
+    res.send(404).send('No Tours found for the specified duration');
+  }
+});
 app.listen(port, () => {
   console.log(`Running on ${port}`);
 });
